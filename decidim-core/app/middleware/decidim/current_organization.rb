9bc02a33dc3ed630c729e282f8e83377081e81b3
Armand <fardeauarmand@gmail.com>
2020-02-06 14:25:29 +0100

    Allow localhost as a host

--- decidim-core/app/middleware/decidim/current_organization.rb
+++ decidim-core/app/middleware/decidim/current_organization.rb
@@ -15,36 +15,49 @@ module Decidim
     #
     # env - A Hash.
     def call(env)
-      detect_current_organization(env)
-      find_secondary_host_org(env)
+      host_for(env)
+      detect_current_organization
+      find_secondary_host_org
 
-      return @app.call(env) if Decidim::Organization.count.zero? && @detect_current_organization.blank? && @find_secondary_host_org.blank?
-      return [404, { "Content-Type" => "text/html", "Content-Length" => "0" }, []] if @detect_current_organization.blank? && @find_secondary_host_org.blank?
+      return @app.call(env) if first_install? || localhost?
 
       if @detect_current_organization
         env["decidim.current_organization"] = @detect_current_organization
         @app.call(env)
-      else
+      elsif @find_secondary_host_org
         location = new_location_for(env, @find_secondary_host_org.host)
 
         [301, { "Location" => location, "Content-Type" => "text/html", "Content-Length" => "0" }, []]
+      else
+        [404, { "Content-Type" => "text/html", "Content-Length" => "0" }, []]
       end
     end
 
     private
 
-    def detect_current_organization(env)
-      host = host_for(env)
-      @detect_current_organization = Decidim::Organization.find_by(host: host)
+    def first_install?
+      Decidim::Organization.count.zero? && no_host?
     end
 
-    def find_secondary_host_org(env)
-      host = host_for(env)
-      @find_secondary_host_org = Decidim::Organization.find_by("? = ANY(secondary_hosts)", host)
+    def no_host?
+      @detect_current_organization.blank? && @find_secondary_host_org.blank?
+    end
+
+    # Respond to curl for heartbeat
+    def localhost?
+      @host == "127.0.0.1" || @host == "localhost"
+    end
+
+    def detect_current_organization
+      @detect_current_organization = Decidim::Organization.find_by(host: @host)
+    end
+
+    def find_secondary_host_org
+      @find_secondary_host_org = Decidim::Organization.find_by("? = ANY(secondary_hosts)", @host)
     end
 
     def host_for(env)
-      Rack::Request.new(env).host.downcase
+      @host = Rack::Request.new(env).host.downcase
     end
 
     def new_location_for(env, host)
